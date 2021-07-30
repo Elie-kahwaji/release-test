00314c0ac4f4167cd555a6416493e19433b6c75b
elie kahwaji <elie.e.kahwaji@gmail.com>
2021-07-30 21:56:24 +0300

    initial commit

---
+++ src/test/java/com/example/demorelease/DemoReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demorelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoReleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
